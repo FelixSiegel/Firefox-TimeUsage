a0ad1e00f6e2a0c5e53bf8ccc5fe206c9f55a0fd
Felix Siegel <ninja.zero@seznam.cz>
2024-01-26 07:04:45 +0100

    Fix for issue #1 code fixed but not tested

--- background-script.js
+++ background-script.js
@@ -4,7 +4,7 @@ console.log("Hello from background!");
 const storageArea = browser.storage.local;
 
 // Function for better logging
-function log(type, msg, color="LawnGreen") {
+function log(type, msg, color = "LawnGreen") {
     console.log(`%c[${type}]: `, `color: ${color};font-weight:bold;`, msg);
 }
 // Function that returns actualy date in forman mm/dd/yy
@@ -17,14 +17,14 @@ function getToday() {
     return (mm + '/' + dd + '/' + yyyy)
 }
 
-function check_url(url, ignore_list=[]) {
+function check_url(url, ignore_list = []) {
     // check if url matches to url-patterns
     var patterns = [/https:\/\/.*\/.*/gm, /http:\/\/.*\/.*/gm]
     for (var i = 0; i < patterns.length; i++) {
         var match = url.match(patterns[i])
-        if (match !== null && match[0] === url) { 
+        if (match !== null && match[0] === url) {
             // check url whether is beeing in ignore-list
-            if (ignore_list.includes(extract_hostname(url))) {return false}
+            if (ignore_list.includes(extract_hostname(url))) { return false }
             return true;
         }
     }
@@ -34,12 +34,12 @@ function check_url(url, ignore_list=[]) {
 function extract_hostname(url) {
     // extract only the hostname of an url
     var idx = url.indexOf("://") + 3;
-    if (idx < 0) {return url}
+    if (idx < 0) { return url }
     var host = "";
     for (var i = idx; i < url.length; i++) {
         if (url[i] != "/") {
             host = host + url[i]
-        } else {break}
+        } else { break }
     }
     return host
 }
@@ -82,23 +82,23 @@ async function deleteTime(url) {
     log("INFO", `Time successfully deleted for ${today}! Url: ${url}`)
 }
 
-async function addTime(url, time, day=null, storage_obj=null) {
-    if (!day) {day = getToday()};
+async function addTime(url, time, day = null, storage_obj = null) {
+    if (!day) { day = getToday() };
     var item = storage_obj ? storage_obj : await storageArea.get(day);
 
-    var obj = {}; 
+    var obj = {};
     obj[day] = item[day] || { [day]: {} };
     obj[day][url] = (item[day][url] ?? 0) + time;
 
-    if (!storage_obj) { 
-        await storageArea.set(obj); 
+    if (!storage_obj) {
+        await storageArea.set(obj);
         log("INFO", `Time successfully added! \nUrl: ${url} | Date: ${day} | Time-added: ${time} | Storage: browser.storage.local`);
-        return new Promise((resolve, _) => {resolve(null)});
+        return new Promise((resolve, _) => { resolve(null) });
     } else {
         for (var key in obj) { item[key] = obj[key] };
         log("INFO", `Time successfully added! \nUrl: ${url} | Date: ${day} | Time-added: ${time} | Storage: given storage-object ${storage_obj}`);
-        return new Promise((resolve, _) => {resolve(item)});
-    }    
+        return new Promise((resolve, _) => { resolve(item) });
+    }
 }
 
 async function addIgnore(url) {
@@ -110,7 +110,7 @@ async function addIgnore(url) {
     var ignore_list = await storageArea.get("ignored");
     var list = (Object.keys(ignore_list).length >= 1) ? ignore_list["ignored"] : [];
     list.push(url);
-    await storageArea.set({"ignored": list});
+    await storageArea.set({ "ignored": list });
     log("INFO", `Url to ignore was added: ${url}`)
 }
 
@@ -120,16 +120,16 @@ async function updateTime(storage_obj = null) {
     var item = await storageArea.get("c_url");
     if (!item?.c_url) {
         log("WARN", "Can't update time! Current url (c_url) is undefined!", "orange");
-        return new Promise((resolve, _) => {resolve(storage_obj)});
+        return new Promise((resolve, _) => { resolve(storage_obj) });
     }
 
     log("INFO", `Updating time for c_url = ${item.c_url}`);
 
     var url = item.c_url[0];
-    var startTime = item.c_url[1]; 
+    var startTime = item.c_url[1];
 
-    var startDate = new Date(startTime*1000); 
-    var curDate = new Date(); 
+    var startDate = new Date(startTime * 1000);
+    var curDate = new Date();
 
     function clearDate(date) {
         // clear time from date
@@ -138,31 +138,42 @@ async function updateTime(storage_obj = null) {
         return clear;
     }
 
+    function isSameDay(date1, date2) {
+        return date1.getFullYear() === date2.getFullYear() &&
+            date1.getMonth() === date2.getMonth() &&
+            date1.getDate() === date2.getDate();
+    }
+
     // If the start date is not today, add times to all days behind the current day
-    if (clearDate(startDate).valueOf() !== clearDate(curDate).valueOf()) {
-        log("WARN", `Start date (${startDate.toLocaleDateString("en-US")}) is not today (${curDate.toLocaleDateString("en-US")}). Adding time to all days behind the current day.`, "orange");
+    if (!isSameDay(startDate, curDate)) {
+        log(
+            "WARN",
+            `Start date (${startDate.toLocaleDateString("en-US")}) is not today`
+            `(${curDate.toLocaleDateString("en-US")}). Adding time to all days behind the current day.`,
+            "orange"
+        );
         var nextDate = new Date(clearDate(startDate));
 
-        while (nextDate < clearDate(curDate)) {
-            // increment date by one day
+        while (!isSameDay(nextDate, curDate)) {
+            // increase date by one day
             nextDate.setDate(nextDate.getDate() + 1);
 
             // get time elapsed in seconds from startDate to nextDate
-            var passedTime = (nextDate - startDate) / 1000 | 0;
-            // add time to day
+            var passedTime = (nextDate.getTime() - startDate.getTime()) / 1000 | 0;
+
             var dd = String(startDate.getDate()).padStart(2, '0');
             var mm = String(startDate.getMonth() + 1).padStart(2, '0');
             var yyyy = startDate.getFullYear();
-            
-            storage_obj = await addTime(url, passedTime, (mm + '/' + dd + '/' + yyyy), storage_obj);
 
-            // increase startDate with passed time to subtract this day
-            startDate.setDate(clearDate(startDate).getDate() + 1);
+            storage_obj = await addTime(url, passedTime, `${mm}/${dd}/${yyyy}`, storage_obj);
+
+            // increase startDate with passed time to subtract this day = start of next day
+            startDate = clearDate(startDate.setDate(startDate.getDate() + 1));
         }
-    } 
+    }
     var elapsedTime = (curDate.getTime() - startDate.getTime()) / 1000 | 0;
     storage_obj = await addTime(url, elapsedTime, null, storage_obj);
-    return new Promise((resolve, _) => {resolve(storage_obj)});
+    return new Promise((resolve, _) => { resolve(storage_obj) });
 }
 
 async function updateActive() {
@@ -181,13 +192,13 @@ async function updateActive() {
 
     // If url is not in whitelist -> return
     if (check === false) {
-        await storageArea.remove("c_url"); 
+        await storageArea.remove("c_url");
         return;
     }
 
     // set current url to storage
     await storageArea.set({
-        "c_url": [extract_hostname(url), Date.now()/1000 | 0]
+        "c_url": [extract_hostname(url), Date.now() / 1000 | 0]
     });
     log("INFO", `Current url (c_url) updated to: ${url}`);
 
@@ -214,13 +225,13 @@ browser.runtime.onMessage.addListener(async (data, _sender, _sendResponse) => {
         // update times in storage
         log("MESSAGE", "update-time-Request received.", "orchid");
         await updateTime();
-        return new Promise((resolve) => { resolve({state: "updated"}) });
-    } 
+        return new Promise((resolve) => { resolve({ state: "updated" }) });
+    }
     else if (data.cmd == 'update_active') {
         // update active tab and time
         log("MESSAGE", "update-active-Request received.", "orchid");
         await updateActive();
-        return new Promise((resolve) => { resolve({state: "updated"}) });
+        return new Promise((resolve) => { resolve({ state: "updated" }) });
     }
     else if (data.cmd == 'stop') {
         // stop time adding by clearing current url
@@ -236,14 +247,14 @@ browser.runtime.onMessage.addListener(async (data, _sender, _sendResponse) => {
         // delete time of entry in browser.storage.local
         log("MESSAGE", "delete-entry-Request received.", "orchid");
         await deleteTime(data.url);
-        return new Promise((resolve) => { resolve({state: "successful"}) });
+        return new Promise((resolve) => { resolve({ state: "successful" }) });
     }
     else if (data.cmd == 'ignore_entry') {
         // add url to ignore_list array in local storage
         log("MESSAGE", "ignore-entry-Request received.", "orchid");
         console.log("Add url to ignore-list: ", data.url);
         await addIgnore(data.url);;
-        return new Promise((resolve) => { resolve({state: "successful"}) });
+        return new Promise((resolve) => { resolve({ state: "successful" }) });
     }
     else if (data.cmd == 'get_storage') {
         // get current storage update it locally and send this new object (it will not be saved in localStorage)
